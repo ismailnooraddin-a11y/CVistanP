27c5e35a418026c3a42a2402561b5b1cb6b46530
ismailnooraddin-a11y <ismailnooraddin@gmail.com>
2026-07-29 21:46:56 +0200

    Delete src directory

--- src/app/meetings/page.tsx
+++
@@ -1,1 +0,0 @@
-import MeetingsModule from '@/modules/meetings/MeetingsModule'; export default function Page(){return <MeetingsModule/>}
--- src/app/page.tsx
+++
@@ -1,2 +0,0 @@
-import Link from 'next/link';
-export default function Home(){return <main className="landing"><div><h1>EstateFlow ERP</h1><p>Multi-branch real estate management platform</p><Link className="btn" href="/login">Sign In</Link></div></main>}
